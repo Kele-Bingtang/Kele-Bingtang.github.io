9508ecb36fe4d8b091dde12df6c8f4388208b5c0
kele_Bingtang <2456019588@qq.com>
2025-05-08 02:46:16 +0800

    新增 bing 收录

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -35,9 +35,9 @@ export default defineConfig({
         content: "width=device-width,initial-scale=1,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no",
       },
     ],
-
     ["meta", { name: "keywords", description }],
     ["meta", { name: "baidu-site-verification", content: "codeva-QnY1Xh758j" }], // 百度收录
+    ["meta", { name: "msvalidate.01", content: "48CABE70F538B8D117567176ABF325AF" }], // Bing 收录验证
   ],
   markdown: {
     // 开启行号
